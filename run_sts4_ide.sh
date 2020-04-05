d3aca5819336d19ad7fccad0656333fe461f4eda
Andy Luis - MVP Java (mvpjava.com) <mvpjava.email@gmail.com>
2020-04-05 22:57:38 +0000

    added git clone to devel env

--- run_sts4_ide.sh
+++ run_sts4_ide.sh
@@ -7,7 +7,6 @@
 #Create directories used as bind mounts volumes on host if they don't exist as current $USER on host in order to have write
 #permissions in Docker container.
 ECLIPSE_DIR=${PWD}/.eclipse     # on host machine
-#ECLIPSE_WORKSPACE_DIR=${PWD}/eclipse-workspace  #on host machine
 ECLIPSE_WORKSPACE_DIR=${PWD}/  #on host machine
 MAVEN_DIR=${HOME}/.m2 #on host machine
 DOCKER_ECLIPSE_WORKSPACE_DIR=/home/mvpjava/Documents/workspace-spring-tool-suite-4-4.6.0.RELEASE
@@ -29,9 +28,7 @@ docker container run -d --rm \
 mvpjava/spring-sts4-ide:jdk14
 
 # When IDE open, import git project ...
-echo "When IDE open, import git project via File->Import->Git->Projects from Git (with smart import)"
-echo "Select Clone URI --> Use the following URI: https://github.com/mvpjava/java-driver-api-mongo4.git"
-echo "Save in directory $DOCKER_ECLIPSE_WORKSPACE_DIR"
-echo "When project is created then run a maven clean package build to get uber jar created in $ECLIPSE_WORKSPACE_DIR "
+echo "When IDE open, import project via File->Import Projects from file systems"
+echo "Choose project $ECLIPSE_WORKSPACE_DIR/java-driver-api-mongo4 "
 
-
+git clone  https://github.com/mvpjava/java-driver-api-mongo4.git java-driver-api-mongo4
