c2b538e93bba1eefc5947c7a8158f5da592916d7
Andy Luis - MVP Java (mvpjava.com) <mvpjava.email@gmail.com>
2020-04-05 22:02:42 +0000

    changed mount path and jar destination

--- run_sts4_ide.sh
+++ run_sts4_ide.sh
@@ -7,7 +7,8 @@
 #Create directories used as bind mounts volumes on host if they don't exist as current $USER on host in order to have write
 #permissions in Docker container.
 ECLIPSE_DIR=${PWD}/.eclipse     # on host machine
-ECLIPSE_WORKSPACE_DIR=${PWD}/eclipse-workspace  #on host machine
+#ECLIPSE_WORKSPACE_DIR=${PWD}/eclipse-workspace  #on host machine
+ECLIPSE_WORKSPACE_DIR=${PWD}/  #on host machine
 MAVEN_DIR=${HOME}/.m2 #on host machine
 DOCKER_ECLIPSE_WORKSPACE_DIR=/home/mvpjava/Documents/workspace-spring-tool-suite-4-4.6.0.RELEASE
 
